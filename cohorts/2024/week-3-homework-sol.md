643b5474eb4555dc48066c3cbb0af6c560998969
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-02-07 17:56:31 -0500

    adding homework sol for week 3

---
+++ cohorts/2024/week-3-homework-sol.md
@@ -0,0 +1,11 @@
+# Week 2 Homework Solution
+## Author: Jonah Oliver
+
+1. 840,402
+2. 0 B processed for External Table and 6.41 MB processed for Materialized Table
+3. 1622
+4. Partition by lpep_pickup_datetime and cluster on PULocationID
+5. 12.82 MB processed for non-[artitioned Table and 1.12 MB processed for partitioned Table
+6. GCP Bucket
+7. False
+8. 0 bytes due to the query being cached
--- my_week_3/hw3_query.sql
+++ my_week_3/hw3_query.sql
@@ -5,7 +5,7 @@ OPTIONS (
   uris = ['gs://mage-zoomcamp-jonah-oliver/green/green_tripdata_2022-*.parquet']
 );
 
--- Check green trip data
+-- Check green trip data test
 SELECT * FROM dtc-de-zoomcamp-410523.ny_taxi.external_green_tripdata;
 
 -- Create a partitioned table from external table
@@ -22,6 +22,14 @@ FROM dtc-de-zoomcamp-410523.ny_taxi.green_tripdata_partitioned;
 -- 840,402
 
 -- Question 2
+-- create materialized table 
+CREATE OR REPLACE TABLE dtc-de-zoomcamp-410523.ny_taxi.materialized_green_tripdata 
+AS SELECT * FROM dtc-de-zoomcamp-410523.ny_taxi.external_green_tripdata;
+
+SELECT COUNT(DISTINCT PULocationID) as distinctPickup FROM dtc-de-zoomcamp-410523.ny_taxi.external_green_tripdata;
+-- 0 B processed 
+SELECT COUNT(DISTINCT PULocationID) as distinctPickup FROM dtc-de-zoomcamp-410523.ny_taxi.materialized_green_tripdata;
+-- 6.41 MB processed 
 
 -- Question 3
 SELECT COUNT(*) FROM dtc-de-zoomcamp-410523.ny_taxi.green_tripdata_partitioned
@@ -29,9 +37,28 @@ WHERE fare_amount = 0;
 -- 1622
 
 -- Question 4
+-- Creating a partition and cluster table
+CREATE OR REPLACE TABLE `dtc-de-zoomcamp-410523.ny_taxi.green_tripdata_partitoned_clustered`
+PARTITION BY DATE(lpep_pickup_datetime)
+CLUSTER BY PULocationID AS
+SELECT * FROM `dtc-de-zoomcamp-410523.ny_taxi.external_green_tripdata`;
+-- Partition by lpep_pickup_datetime and cluster on PULocationID
 
 -- Question 5
+SELECT DISTINCT(PULocationID) as distinctPickup FROM dtc-de-zoomcamp-410523.ny_taxi.materialized_green_tripdata
+WHERE DATE(lpep_pickup_datetime) BETWEEN '2022-06-01' AND '2022-06-30';
+-- 12.82 MB processed
+
+SELECT DISTINCT(PULocationID) as distinctPickup FROM dtc-de-zoomcamp-410523.ny_taxi.green_tripdata_partitoned_clustered
+WHERE DATE(lpep_pickup_datetime) BETWEEN '2022-06-01' AND '2022-06-30';
+-- 1.12 MB processed
 
 -- Question 6
+-- GCP Bucket 
 
 -- Question 7
+-- FALSE
+
+-- Question 8
+SELECT count(*) as query FROM dtc-de-zoomcamp-410523.ny_taxi.materialized_green_tripdata;
+-- 0 bytes due to the query being cached
